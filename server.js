2f052ec19327742b95b26fe8332779b1aee09d8b
RoshanKhatri0 <lastkhatri@gmail.com>
2024-01-09 14:12:56 +0545

    port

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.use('/api',productRoute)
 
 const port=process.env.PORT || 5000
 app.listen(port,()=>{
-    console.log(`Server started on port ${port}`)
+    console.log(`Server started on port http://localhost:${port}`)
 })
