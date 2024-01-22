7a19f7b13dbebcc9486ec34469158ef52c4325bf
RoshanKhatri0 <lastkhatri@gmail.com>
2024-01-22 14:36:07 +0545

    order model

--- models/orderModel.js
+++ models/orderModel.js
@@ -11,7 +11,7 @@ const orderSchema = new mongoose.Schema({
         type:String,
         required:true   
     },
-    shippingAddress:{
+    shippingAddress2:{
         type:String,
     },
     city:{
